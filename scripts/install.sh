d932b8edf81a532579ae8dbda9a5f83425dc3234
GoMinerProxy <96786365+GoMinerProxy@users.noreply.github.com>
2021-12-31 17:35:59 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ mkdir go_miner_proxy
 wget https://raw.githubusercontent.com/GoMinerProxy/GoMinerProxy/main/scripts/run.sh -O ./go_miner_proxy/run.sh
 chmod 777 ./go_miner_proxy/run.sh
 
-wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.0.2/GoMinerProxy_v1.0.2_linux_amd64.tar.gz
+wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.0.2/GoMinerProxy_v1.0.2_linux_amd64.tar.gz -O GoMinerProxy_v1.0.2_linux_amd64.tar.gz
 tar -zxvf GoMinerProxy_v1.0.2_linux_amd64.tar.gz -C ./go_miner_proxy
 chmod 777 ./go_miner_proxy/GoMinerProxy
 
