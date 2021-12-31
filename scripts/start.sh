8ed4c10eef44059d3ffd6ee05875afa1fbb2d21f
GoMinerProxy <96786365+GoMinerProxy@users.noreply.github.com>
2021-12-31 17:34:06 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+screen -X -S go_miner_proxy quit
+
 screen -dmS go_miner_proxy
 sleep 0.2s
-screen -r go_miner_proxy -p 0 -X stuff "bash ./go_miner_proxy/run.sh"
+screen -r go_miner_proxy -p 0 -X stuff "cd go_miner_proxy"
+screen -r go_miner_proxy -p 0 -X stuff $'\n'
+screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
