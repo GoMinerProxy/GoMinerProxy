75bfd60d2ac587a3eee7030214a059427258924a
GoMinerProxy <96786365+GoMinerProxy@users.noreply.github.com>
2021-12-31 17:48:47 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,3 +7,6 @@ screen -r go_miner_proxy -p 0 -X stuff "cd go_miner_proxy"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
 screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
+
+echo "GoMinerProxy已启动"
+echo "您可以使用指令screen -r go_miner_proxy查看程序輸出" 
