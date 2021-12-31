8cb3d7546d438d0f1e5609b270dff609fa11ccc2
GoMinerProxy <96786365+GoMinerProxy@users.noreply.github.com>
2021-12-31 17:46:51 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,3 +13,8 @@ screen -r go_miner_proxy -p 0 -X stuff "cd go_miner_proxy"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
 screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
+
+echo "GoMinerProxy 已经安装完成并已启动"
+echo "已启动网页后台，默认端口 9998，密码 admin"
+echo "请您及时连接网页后台并修改默认密码，防止被人爆破"
+echo "您可以使用指令screen -r go_miner_proxy查看程序输出"
