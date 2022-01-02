cb30bdc9ea1a1b5f6caf62defd1579f94bf543ec
GoMinerProxy <minerproxy@outlook.com>
2022-01-02 00:11:13 +0000

    release 1.0.5

--- others/announcement.md
+++ others/announcement.md
@@ -1,2 +1,2 @@
-#### 最新版本：V1.0.4(2022-01-01)
+#### 最新版本：V1.0.5(2022-01-02)
 #### 聯繫我們：[Telegram 討論群組](https://t.me/+afVqEXnxtQAyNWNh)、[GitHub](https://github.com/GoMinerProxy/GoMinerProxy) 
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.0.4/GoMinerProxy_v1.0.4_linux_amd64.tar.gz -O GoMinerProxy_v1.0.4_linux_amd64.tar.gz
+wget https://github.com/GoMinerProxy/GoMinerProxy/releases/download/1.0.5/GoMinerProxy_v1.0.5_linux_amd64.tar.gz -O GoMinerProxy_v1.0.5_linux_amd64.tar.gz
 
 screen -X -S go_miner_proxy quit
 rm -rf ./go_miner_proxy/GoMinerProxy
 
-tar -zxvf GoMinerProxy_v1.0.4_linux_amd64.tar.gz -C ./go_miner_proxy
+tar -zxvf GoMinerProxy_v1.0.5_linux_amd64.tar.gz -C ./go_miner_proxy
 chmod 777 ./go_miner_proxy/GoMinerProxy
 
 screen -dmS go_miner_proxy
@@ -14,5 +14,5 @@ screen -r go_miner_proxy -p 0 -X stuff $'\n'
 screen -r go_miner_proxy -p 0 -X stuff "./run.sh"
 screen -r go_miner_proxy -p 0 -X stuff $'\n'
 
-echo "GoMinerProxy 已經更新至V1.0.3版本並啟動"
+echo "GoMinerProxy 已經更新至V1.0.5版本並啟動"
 echo "您可以使用指令screen -r go_miner_proxy查看程序輸出"
